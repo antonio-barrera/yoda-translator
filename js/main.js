d1dc04e192830857b94c3f9704089986670d3082
Antonio Barrera <jeantonio.barrera@gmail.com>
2021-09-21 16:35:24 -0600

    added components funcionality

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var txtInput = document.querySelector("#input-text");
+var txtOutput = document.querySelector("#output-text");
+var btnTranslate = document.querySelector("#translate-button");
+
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/yoda.json";
+
+function constructURL(text) {
+    return serverURL + "?text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("Something was wrong", error);
+    alert("Try again later...");
+} 
+
+function eventHandler() {
+    var inputText = txtInput.value;
+    fetch(constructURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        txtOutput.innerText = translatedText;
+    })
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", eventHandler);
